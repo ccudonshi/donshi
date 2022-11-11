fe952202e34ede110960568d8d6d0d27313ba991
lo0k0502 <thomas85514@gmail.com>
2022-11-12 05:21:37 +0800

    update notifications

--- src/helper/notification.js
+++ src/helper/notification.js
@@ -1,47 +1,47 @@
 import React from 'react';
-import { Platform } from 'react-native';
-import { Notifications } from 'expo';
-import * as Permissions from 'expo-permissions';
-import Constants from 'expo-constants';
-export async function notify(message,type="local") {
-  if(type === "local")Notifications.presentLocalNotificationAsync(message)
-  else {
-    const response = await fetch('https://exp.host/--/api/v2/push/send', {
-      method: 'POST',
-      headers: {
-          Accept: 'application/json',
-          'Accept-encoding': 'gzip, deflate',
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(message),
+import { Alert, Platform } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+
+/**
+ * @param {Notifications.NotificationContentInput} content
+ */
+export const notify = async (content) => {
+  await Notifications.scheduleNotificationAsync({
+    content,
+    trigger: null,
+  });
+};
+
+export const registerForPushNotificationsAsync = async () => {
+  let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
     });
   }
-}
-export async function registerForPushNotificationsAsync(setExpoPushToken){
-  if (Constants.isDevice) {
-    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
     let finalStatus = existingStatus;
     if (existingStatus !== 'granted') {
-      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      const { status } = await Notifications.requestPermissionsAsync();
       finalStatus = status;
     }
     if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
+      Alert.alert('Failed to get push token for push notification!');
       return;
     }
-    token = await Notifications.getExpoPushTokenAsync();
+    token = (await Notifications.getExpoPushTokenAsync()).data;
     console.log(token);
-    setExpoPushToken(token)
   } else {
     alert('Must use physical device for Push Notifications');
+    Alert.alert('Must use physical device for Push Notifications');
   }
 
-  if (Platform.OS === 'android') {
-    Notifications.createChannelAndroidAsync('default', {
-      name: 'default',
-      sound: true,
-      priority: 'max',
-      vibrate: [0, 250, 250, 250],
-    });
-  }
+  return token;
 };
